8e302ffebd55d7b08aeb17830e38a2c17035ee97
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 19:15:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++) {
-    return `"I am ${i} strange loop${i.length === 0 || 1 ? '' : 's'}."`
+    console.log(`"I am ${i} strange loop${i.length === 0 || 1 ? '' : 's'}."`)
   }
 }
